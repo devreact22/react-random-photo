e28245fc45dd308dcd188b55dad483c1d2800dea
Dev <117921993+devreact22@users.noreply.github.com>
2024-06-23 08:42:55 +0200

    fix git ignote e vite

--- src/components/Fetch-data.js
+++ src/components/Fetch-data.js
@@ -14,6 +14,7 @@ function RandomPhotos() {
       setTimeout(async () => {
         const response = await fetch('https://picsum.photos/v2/list');
         const data = await response.json();
+        console.log(data);
         setPhotos(data);
         setIsLoading(false);
       }, 1000);
